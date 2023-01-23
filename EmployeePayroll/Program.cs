91a7ddada4e92f0e0b56b04777b57674787ed770
aditya patil <Aditya@example.com>
2023-01-23 21:30:19 +0530

    [Aditya]Add:Created A Database

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,20 @@
+﻿using EmployeePayroll.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EmplyoyeePayroll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeRepository employee = new EmployeeRepository();
+            employee.CreateDatabase();
+        }
+    }
+}
---
+++ EmployeePayroll/Repository/EmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll.Repository
+{
+    public class EmployeeRepository
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-835JJ2N;Initial catalog=master;
+                                  Integrated Security=true");
+        public void CreateDatabase()
+        {
+
+            SqlCommand cmd = new SqlCommand("Create database EmpPayroll", connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Database created successfully");
+        }
+    }
+}
